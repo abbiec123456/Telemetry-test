08c66deebc59944cabd1840ef50fe9c7385dfa23
Abbie <L00196596@atu.ie>
2025-12-17 19:44:47 +0000

    up

---
+++ src/public class HashUtil {.java
@@ -0,0 +1,17 @@
+public class HashUtil {
+
+    public static String md5(String input) {
+        try {
+            var md = java.security.MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(input.getBytes());
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
